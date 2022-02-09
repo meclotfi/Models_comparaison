2624328438f5e29b81b367d33520b3b7b66f0fa1
Meclotfi <hl_mecharbat@esi.dz>
2022-02-09 11:47:13 +0100

    Update comp.sh

--- comp.sh
+++ comp.sh
@@ -2,7 +2,7 @@
 
 for Name in "Swin" "Detr-R50" "Def-DETR-R50" "Def-DETR-R50+iterative_bbox" "Def-DETR-R50++two_stage" "PVT-Tiny" "PVTv2-B0"
 do
-  for Bs in 1
+  for Bs in 1 4 8 16
   do
      python3 ./run_model.py $Name $Bs
       
